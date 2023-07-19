a5afc6bd535b1b43dc65f3e61bae3f8e90dff584
zcl <41391477@qq.com>
2023-07-19 21:41:44 +0800

    add-api

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="page-container">
 		<u-action-sheet :list="list" v-model="show"></u-action-sheet>
-		<u-button type="warning" text="月落"></u-button>
+		<u-button type="warning" text="月落" @click="callVKFunction"></u-button>
 	</view>
 </template>
 
@@ -20,8 +20,23 @@
 			text: '评论'
 		}
 	]);
-	
+	const callVKFunction = () => {
+		vk.callFunction({
+			url: 'client/pub.user.calc',
+			title: '请求中...',
+			data: {
+				x: 2,
+				y: 4
+			},
+			success: (data) => {
+				// 在这里处理成功的响应
+			}
+		});
+	};
 	const show = ref(true);
+	defineExpose({
+		callVKFunction
+	});
 	onMounted(async () => {
 		
 	});
